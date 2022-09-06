d9161d7efb24d19762f4a340d306076b5b90ae87
teguhmuhams <teguhmuhams@gmail.com>
2022-09-06 21:57:21 +0700

    Add Todolist component and action

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Livewire\Calculator;
+use App\Http\Livewire\TodoList;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,3 +21,4 @@ Route::redirect('/', 'counter');
 Route::view('/counter', 'welcome')->name('counter');
 
 Route::get('/calculator', Calculator::class)->name('calculator');
+Route::get('/todo-list', TodoList::class)->name('todo-list');
