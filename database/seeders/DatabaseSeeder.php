f278911c9919c99495a508f202e076e45abcc7e5
teguhmuhams <teguhmuhams@gmail.com>
2022-09-09 08:48:44 +0700

    Add cascading dropdown component

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Continent;
+use App\Models\Country;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,11 +17,18 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $continents = [
+            ['id' => 1, 'name' => 'Asia'],
+            ['id' => 2, 'name' => 'Europe'],
+            ['id' => 3, 'name' => 'Africa'],
+            ['id' => 4, 'name' => 'America'],
+            ['id' => 5, 'name' => 'Australia']
+        ];
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        foreach ($continents as $continent) {
+            Continent::factory()->create($continent)->each(function ($c) {
+                $c->countries()->saveMany(Country::factory(10)->make());
+            });
+        }
     }
 }
